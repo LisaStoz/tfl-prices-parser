b97f66916ac1e375024a91f8ba5ea0971a2e19ad
LisaStoz <lisa@lisastoz.com>
2014-01-18 21:13:07 +0000

    db update

--- index.php
+++ index.php
@@ -1,36 +1,47 @@
 <?php
 
-    function readSheet($Reader, $sheetNr, $functionName) {
+    function readSheet($db, $Reader, $sheetNr, $functionName) {
         $Reader -> ChangeSheet($sheetNr);       
         $counter = 0;
         foreach ($Reader as $row) {
             $counter++;
-            if ($counter > 1) {            
+            if ($counter > 1) {  
+                for ($i = 0; $i <= 8; $i++) {
+                    if ($row[$i] == '')
+                        $row[$i] = 0;
+                }
+                    
                 $wheres = $functionName($row, 2, 0);
-                update_price($wheres, $row[4]);
+                update_price($db, $wheres, $row[4]);
                 $wheres = $functionName($row, 2, 1);
-                update_price($wheres, $row[5]);
+                update_price($db, $wheres, $row[5]);
                 $wheres = $functionName($row, 2, 2);
-                update_price($wheres, $row[6]);
+                update_price($db, $wheres, $row[6]);
                 $wheres = $functionName($row, 2, 3);
-                update_price($wheres, $row[7]);
+                update_price($db, $wheres, $row[7]);
                 $wheres = $functionName($row, 4, 0);
-                update_price($wheres, $row[8]);
+                update_price($db, $wheres, $row[8]);
                 $wheres = $functionName($row, 4, 4);
-                update_price($wheres, $row[9]);
+                update_price($db, $wheres, $row[9]);
                 $wheres = $functionName($row, 6, 0);
-                update_price($wheres, $row[10]);
+                update_price($db, $wheres, $row[10]);
                 $wheres = $functionName($row, 7, 0);
-                update_price($wheres, $row[11]);
+                update_price($db, $wheres, $row[11]);
                 $wheres = $functionName($row, 8, 0);
-                update_price($wheres, $row[12]);
+                update_price($db, $wheres, $row[12]);
             }           
         }
     }
     
-    function update_price($wheres, $value) {
-        print_r($wheres);
-        echo $value.'<br>';
+    function update_price($db, $wheres, $value) {
+        $parts = array();        
+        foreach ($wheres as $column=>$v) {
+            array_push($parts, $column."='".$v."'");            
+        }
+       
+        $sql = "UPDATE prices SET value = '".$value."' WHERE ".implode(" AND ", $parts);                             
+        $sth = $db->prepare($sql);
+        $sth->execute(array($value));       
     }
     
        
@@ -73,35 +84,35 @@
 
     /* underground */
     function underground_adult($row, $payType, $conditionalDiscount) {       
-        $wheres = underground($row, 0);
-        $wheres['pay_type'] = $payType;
-        $wheres['conditional_discount'] = $conditionalDiscount;
-        return $wheres;
-    }
-    
-    function underground_18($row, $payType, $conditionalDiscount) {
         $wheres = underground($row, 1);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
     }
     
-    function underground_16($row, $payType, $conditionalDiscount) {
+    function underground_18($row, $payType, $conditionalDiscount) {
         $wheres = underground($row, 2);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
     }
     
-    function underground_11($row, $payType, $conditionalDiscount) {
+    function underground_16($row, $payType, $conditionalDiscount) {
         $wheres = underground($row, 3);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
     }
     
+    function underground_11($row, $payType, $conditionalDiscount) {
+        $wheres = underground($row, 4);
+        $wheres['pay_type'] = $payType;
+        $wheres['conditional_discount'] = $conditionalDiscount;
+        return $wheres;
+    }
+    
     function underground_jobcentre($row, $payType, $conditionalDiscount) {
-        $wheres = underground($row, 5);
+        $wheres = underground($row, 7);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
@@ -117,42 +128,42 @@
     /* rail */
     
     function rail_adult($row, $payType, $conditionalDiscount) {       
-        $wheres = rail($row, 0);
-        $wheres['pay_type'] = $payType;
-        $wheres['conditional_discount'] = $conditionalDiscount;
-        return $wheres;
-    }
-    
-    function rail_18($row, $payType, $conditionalDiscount) {
         $wheres = rail($row, 1);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
     }
     
-    function rail_16($row, $payType, $conditionalDiscount) {
+    function rail_18($row, $payType, $conditionalDiscount) {
         $wheres = rail($row, 2);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
     }
     
-    function rail_11($row, $payType, $conditionalDiscount) {
+    function rail_16($row, $payType, $conditionalDiscount) {
         $wheres = rail($row, 3);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
     }
     
-    function rail_5($row, $payType, $conditionalDiscount) {
+    function rail_11($row, $payType, $conditionalDiscount) {
         $wheres = rail($row, 4);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
     }
     
-    function rail_jobcentre($row, $payType, $conditionalDiscount) {
+    function rail_5($row, $payType, $conditionalDiscount) {
         $wheres = rail($row, 5);
+        $wheres['pay_type'] = $payType;
+        $wheres['conditional_discount'] = $conditionalDiscount;
+        return $wheres;
+    }
+    
+    function rail_jobcentre($row, $payType, $conditionalDiscount) {
+        $wheres = rail($row, 7);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
@@ -168,42 +179,42 @@
     /* rail through */
     
     function railthrough_adult($row, $payType, $conditionalDiscount) {       
-        $wheres = railthrough($row, 0);
-        $wheres['pay_type'] = $payType;
-        $wheres['conditional_discount'] = $conditionalDiscount;
-        return $wheres;
-    }
-    
-    function railthrough_18($row, $payType, $conditionalDiscount) {
         $wheres = railthrough($row, 1);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
     }
     
-    function railthrough_16($row, $payType, $conditionalDiscount) {
+    function railthrough_18($row, $payType, $conditionalDiscount) {
         $wheres = railthrough($row, 2);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
     }
     
-    function railthrough_11($row, $payType, $conditionalDiscount) {
+    function railthrough_16($row, $payType, $conditionalDiscount) {
         $wheres = railthrough($row, 3);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
     }
     
-    function railthrough_5($row, $payType, $conditionalDiscount) {
+    function railthrough_11($row, $payType, $conditionalDiscount) {
         $wheres = railthrough($row, 4);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
     }
     
-    function railthrough_jobcentre($row, $payType, $conditionalDiscount) {
+    function railthrough_5($row, $payType, $conditionalDiscount) {
         $wheres = railthrough($row, 5);
+        $wheres['pay_type'] = $payType;
+        $wheres['conditional_discount'] = $conditionalDiscount;
+        return $wheres;
+    }
+    
+    function railthrough_jobcentre($row, $payType, $conditionalDiscount) {
+        $wheres = railthrough($row, 7);
         $wheres['pay_type'] = $payType;
         $wheres['conditional_discount'] = $conditionalDiscount;
         return $wheres;
@@ -217,6 +228,8 @@
     }
 
     /* ---------------------------------------------- */
+    
+    $db = new PDO('mysql:host=localhost;dbname=farecalc_backup_20140118;charset=UTF-8', 'root', '');
 
 	require('../spreadsheet-reader-master/php-excel-reader/excel_reader2.php');
     require('../spreadsheet-reader-master/SpreadsheetReader.php');
@@ -225,39 +238,38 @@
     $Sheets = $Reader -> Sheets();
          
     
-    readSheet($Reader, 0, 'underground_adult');
-    readSheet($Reader, 1, 'underground_18');
-    readSheet($Reader, 2, 'underground_16');
-    readSheet($Reader, 3, 'underground_11');
-    readSheet($Reader, 4, 'underground_jobcentre');
-    readSheet($Reader, 5, 'underground_adult');
-    readSheet($Reader, 6, 'underground_railcard');
-    
-    
-    
+    readSheet($db, $Reader, 0, 'underground_adult');   
+    readSheet($db, $Reader, 1, 'underground_18');
+    readSheet($db, $Reader, 2, 'underground_16');
+    readSheet($db, $Reader, 3, 'underground_11');
+    readSheet($db, $Reader, 4, 'underground_jobcentre');
+    readSheet($db, $Reader, 5, 'underground_adult');
+    readSheet($db, $Reader, 6, 'underground_railcard');
+   
+    exit;
     $Reader = new SpreadsheetReader('../resources/rail_only.xls');
     $Sheets = $Reader -> Sheets();
          
-    readSheet($Reader, 0, 'rail_adult');
-    readSheet($Reader, 1, 'rail_18');
-    readSheet($Reader, 2, 'rail_16');
-    readSheet($Reader, 3, 'rail_11');
-    readSheet($Reader, 3, 'rail_5');
-    readSheet($Reader, 4, 'rail_jobcentre');
-    readSheet($Reader, 5, 'rail_adult');
-    readSheet($Reader, 6, 'rail_railcard');
+    readSheet($db, $Reader, 0, 'rail_adult');
+    readSheet($db, $Reader, 1, 'rail_18');
+    readSheet($db, $Reader, 2, 'rail_16');
+    readSheet($db, $Reader, 3, 'rail_11');
+    readSheet($db, $Reader, 3, 'rail_5');
+    readSheet($db, $Reader, 4, 'rail_jobcentre');
+    readSheet($db, $Reader, 5, 'rail_adult');
+    readSheet($db, $Reader, 6, 'rail_railcard');
    
     $Reader = new SpreadsheetReader('../resources/rail_through.xls');
     $Sheets = $Reader -> Sheets();
          
-    readSheet($Reader, 0, 'railthrough_adult');
-    readSheet($Reader, 1, 'railthrough_18');
-    readSheet($Reader, 2, 'railthrough_16');
-    readSheet($Reader, 3, 'railthrough_11');
-    readSheet($Reader, 3, 'railthrough_5');
-    readSheet($Reader, 4, 'railthrough_jobcentre');
-    readSheet($Reader, 5, 'railthrough_adult');
-    readSheet($Reader, 6, 'railthrough_railcard');
+    readSheet($db, $Reader, 0, 'railthrough_adult');
+    readSheet($db, $Reader, 1, 'railthrough_18');
+    readSheet($db, $Reader, 2, 'railthrough_16');
+    readSheet($db, $Reader, 3, 'railthrough_11');
+    readSheet($db, $Reader, 3, 'railthrough_5');
+    readSheet($db, $Reader, 4, 'railthrough_jobcentre');
+    readSheet($db, $Reader, 5, 'railthrough_adult');
+    readSheet($db, $Reader, 6, 'railthrough_railcard');
    
    
 ?>
